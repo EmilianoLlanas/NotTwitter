968936b88a6856395e6c3d54b7c20eca2a85a99b
Samantha Estrada <samantha.estrada27@gmail.com>
2021-11-29 16:50:37 -0600

    manejo de sesion

--- src/main/java/com/action/MenuAction.java
+++ src/main/java/com/action/MenuAction.java
@@ -1,8 +1,28 @@
 package com.action;
 
-public class MenuAction {
+import java.util.Map;
 
+import org.apache.struts2.interceptor.SessionAware;
+
+import com.model.Usuario;
+
+public class MenuAction implements SessionAware{
+
+	Map<String, Object> session;
+	
+	@Override
+	public void setSession(Map<String, Object> session) {
+		// TODO Auto-generated method stub
+		this.session=session;
+	}
+	
 	public String execute() {
+		String ret = "error";	
+		Usuario userLogin = (Usuario) session.get("user");
+		if(userLogin == null)
+			return ret;
 		return "success";
 	}
 }
+
+	
