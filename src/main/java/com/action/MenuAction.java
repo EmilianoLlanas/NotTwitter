2a943fe36fb20e6137c37721c634ee041309b4b5
Samantha Estrada <samantha.estrada27@gmail.com>
2021-11-30 15:42:50 -0600

    correccion menuaction

--- src/main/java/com/action/MenuAction.java
+++ src/main/java/com/action/MenuAction.java
@@ -1,26 +1,10 @@
 package com.action;
 
-import java.util.Map;
+public class MenuAction{
 
-import org.apache.struts2.interceptor.SessionAware;
-
-import com.model.Usuario;
-
-public class MenuAction implements SessionAware{
-
-	Map<String, Object> session;
-	
-	@Override
-	public void setSession(Map<String, Object> session) {
-		// TODO Auto-generated method stub
-		this.session=session;
-	}
 	
 	public String execute() {
-		String ret = "error";	
-		Usuario userLogin = (Usuario) session.get("user");
-		if(userLogin == null)
-			return ret;
+
 		return "success";
 	}
 }
