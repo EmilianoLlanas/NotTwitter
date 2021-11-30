c497398e42d5a369f70f8f11ba2181ea161960c1
Samantha Estrada <samantha.estrada27@gmail.com>
2021-11-30 14:33:21 -0600

    menuaction

--- src/main/java/com/action/MenuAction.java
+++ src/main/java/com/action/MenuAction.java
@@ -5,6 +5,10 @@ public class MenuAction {
 
 	
 	public String execute() {
+		/*String ret = "error";	
+		Usuario userLogin = (Usuario) session.get("user");
+		if(userLogin == null)
+			return ret;*/
 		
 		return "success";
 	}
